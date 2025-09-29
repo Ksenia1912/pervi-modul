753e7218df905e9eb5fa4ca23adaf2a3b1466401
ksyu6 <ksyu6@DESPOIN>
2025-09-29 10:36:53 +0300

    Добавьте файлы проекта.

---
+++ 1.1 3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите ваше имя: ");
+        string Name = Console.ReadLine();
+
+        Console.Write("Введите вашу фамилию: ");
+        string Familia = Console.ReadLine();
+
+        Console.WriteLine($"Результат: {Familia}, {Name}");
+    }
+}
