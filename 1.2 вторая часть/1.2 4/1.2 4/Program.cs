6909178b4ee395e8d254fbdda493a4d132a1f272
Ksenia1912 <ksyu6ka07@gmail.com>
2025-10-01 11:23:25 +0300

    Add files via upload

---
+++ 1.2 вторая часть/1.2 4/1.2 4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер массива K: ");
+        int k = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите начало диапазона A: ");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите конец диапазона B: ");
+        int b = int.Parse(Console.ReadLine());
+
+        Random random = new Random();
+        int[] array = new int[k];
+
+        // Заполнение массива случайными числами
+        for (int i = 0; i < k; i++)
+        {
+            array[i] = random.Next(a, b);
+        }
+
+        // Поиск индексов минимального и максимального элементов
+        int minIndex = 0;
+        int maxIndex = 0;
+
+        for (int i = 1; i < k; i++)
+        {
+            if (array[i] < array[minIndex])
+                minIndex = i;
+            if (array[i] > array[maxIndex])
+                maxIndex = i;
+        }
+
+        // Определение границ для вывода
+        int start = Math.Min(minIndex, maxIndex);
+        int end = Math.Max(minIndex, maxIndex);
+
+        // Вывод результатов
+        Console.WriteLine("Массив:");
+        for (int i = 0; i < k; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Минимальный элемент: array[{minIndex}] = {array[minIndex]}");
+        Console.WriteLine($"Максимальный элемент: array[{maxIndex}] = {array[maxIndex]}");
+
+        Console.WriteLine("Элементы между минимальным и максимальным (включительно):");
+        for (int i = start; i <= end; i++)
+        {
+            Console.Write(array[i] + " ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ 1.2 первая часть/1.2 1/1.2 1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите размер массива N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        double[] array = new double[n];
+
+        // Ввод элементов массива
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введите элемент [{i}]: ");
+            array[i] = double.Parse(Console.ReadLine());
+        }
+
+        // Поиск максимального по модулю элемента
+        double maxAbs = Math.Abs(array[0]);
+        for (int i = 1; i < n; i++)
+        {
+            if (Math.Abs(array[i]) > maxAbs)
+                maxAbs = Math.Abs(array[i]);
+        }
+
+        // Нормировка массива
+        for (int i = 0; i < n; i++)
+        {
+            array[i] /= maxAbs;
+        }
+
+        // Вывод результата
+        Console.WriteLine("Нормированный массив:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"array[{i}] = {array[i]:F4}");
+        }
+    }
+}
