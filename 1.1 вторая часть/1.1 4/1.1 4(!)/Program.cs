b35112f49c94cc5919a0866a6b8c3d49d0403f24
Ksenia1912 <ksyu6ka07@gmail.com>
2025-10-01 11:21:54 +0300

    Add files via upload

---
+++ 1.1 вторая часть/1.1 4/1.1 4(!)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int[] numbers = new int[20];
+
+        // Заполнение массива случайными числами
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            numbers[i] = random.Next(1, 101); // числа от 1 до 100
+        }
+
+        // Поиск минимального и максимального значения
+        int min = numbers[0];
+        int max = numbers[0];
+
+        for (int i = 1; i < numbers.Length; i++)
+        {
+            if (numbers[i] < min)
+                min = numbers[i];
+            if (numbers[i] > max)
+                max = numbers[i];
+        }
+
+        // Вывод результатов
+        Console.WriteLine("Массив чисел:");
+        foreach (int number in numbers)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine($"\nМинимальное значение: {min}");
+        Console.WriteLine($"Максимальное значение: {max}");
+    }
+}
---
+++ 1.1 первая часть/1.1 1/1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"Число {number} является чётным");
+        }
+        else
+        {
+            Console.WriteLine($"Число {number} является нечётным");
+        }
+    }
+}
