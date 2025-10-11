9769a53616779acd3d9f3f4468efb0570e65c440
Ksenia1912 <ksyu6ka07@gmail.com>
2025-10-11 08:14:44 +0300

    Add files via upload

---
+++ 4 вторая часть/4.4/4.4/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+
+// Интерфейс книги
+interface IBook
+{
+    bool IsAvailable();
+    void Borrow();
+    void Return();
+}
+
+// Обычная книга
+class Book : IBook
+{
+    public string Title;
+    public string Author;
+    public bool IsBorrowed;
+
+    public bool IsAvailable()
+    {
+        return !IsBorrowed;
+    }
+
+    public void Borrow()
+    {
+        if (!IsBorrowed)
+        {
+            IsBorrowed = true;
+            Console.WriteLine($"Книга '{Title}' выдана");
+        }
+        else
+        {
+            Console.WriteLine($"Книга '{Title}' уже выдана");
+        }
+    }
+
+    public void Return()
+    {
+        if (IsBorrowed)
+        {
+            IsBorrowed = false;
+            Console.WriteLine($"Книга '{Title}' возвращена");
+        }
+        else
+        {
+            Console.WriteLine($"Книга '{Title}' уже в библиотеке");
+        }
+    }
+}
+
+// Учебник
+class Textbook : IBook
+{
+    public string Title;
+    public string Author;
+    public string Subject;
+    public bool IsBorrowed;
+
+    public bool IsAvailable()
+    {
+        return !IsBorrowed;
+    }
+
+    public void Borrow()
+    {
+        if (!IsBorrowed)
+        {
+            IsBorrowed = true;
+            Console.WriteLine($"Учебник '{Title}' по предмету {Subject} выдан");
+        }
+        else
+        {
+            Console.WriteLine($"Учебник '{Title}' уже выдан");
+        }
+    }
+
+    public void Return()
+    {
+        if (IsBorrowed)
+        {
+            IsBorrowed = false;
+            Console.WriteLine($"Учебник '{Title}' возвращен");
+        }
+        else
+        {
+            Console.WriteLine($"Учебник '{Title}' уже в библиотеке");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Библиотека:");
+
+        // Обычная книга
+        Book book1 = new Book();
+        book1.Title = "Кровавая корона";
+        book1.Author = "Шуи Цянчэн";
+        book1.IsBorrowed = false;
+
+        // Учебник
+        Textbook textbook1 = new Textbook();
+        textbook1.Title = "Математика";
+        textbook1.Author = "Иванов";
+        textbook1.Subject = "Математика";
+        textbook1.IsBorrowed = false;
+
+        // Проверяем доступность
+        Console.WriteLine($"Книга '{book1.Title}' доступна: {book1.IsAvailable()}");
+        Console.WriteLine($"Учебник '{textbook1.Title}' доступен: {textbook1.IsAvailable()}");
+
+        // Выдаем книги
+        book1.Borrow();
+        textbook1.Borrow();
+
+        // Пытаемся выдать снова
+        book1.Borrow();
+
+        // Возвращаем книгу
+        book1.Return();
+
+        // Проверяем снова
+        Console.WriteLine($"Книга '{book1.Title}' доступна: {book1.IsAvailable()}");
+    }
+}
---
+++ 4 вторая часть/4.5/4.5/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+// Интерфейс для рисования
+interface IDrawing
+{
+    void Draw();
+    void Clear();
+}
+
+// Класс для рисования линий
+class LineDrawing : IDrawing
+{
+    public int X1, Y1, X2, Y2;
+
+    public void Draw()
+    {
+        Console.WriteLine($"Рисую линию от ({X1},{Y1}) до ({X2},{Y2})");
+    }
+
+    public void Clear()
+    {
+        Console.WriteLine("Стираю линию");
+    }
+}
+
+// Класс для рисования кругов
+class CircleDrawing : IDrawing
+{
+    public int X, Y, Radius;
+
+    public void Draw()
+    {
+        Console.WriteLine($"Рисую круг в точке ({X},{Y}) с радиусом {Radius}");
+    }
+
+    public void Clear()
+    {
+        Console.WriteLine("Стираю круг");
+    }
+}
+
+// Класс для рисования прямоугольников
+class RectangleDrawing : IDrawing
+{
+    public int X, Y, Width, Height;
+
+    public void Draw()
+    {
+        Console.WriteLine($"Рисую прямоугольник в ({X},{Y}) размером {Width}x{Height}");
+    }
+
+    public void Clear()
+    {
+        Console.WriteLine("Стираю прямоугольник");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Рисование на холсте:");
+
+        // Рисуем линию
+        LineDrawing line = new LineDrawing();
+        line.X1 = 10;
+        line.Y1 = 10;
+        line.X2 = 50;
+        line.Y2 = 50;
+        line.Draw();
+
+        // Рисуем круг
+        CircleDrawing circle = new CircleDrawing();
+        circle.X = 30;
+        circle.Y = 30;
+        circle.Radius = 20;
+        circle.Draw();
+
+        // Рисуем прямоугольник
+        RectangleDrawing rect = new RectangleDrawing();
+        rect.X = 5;
+        rect.Y = 5;
+        rect.Width = 40;
+        rect.Height = 30;
+        rect.Draw();
+
+        // Стираем
+        Console.WriteLine("\nСтираем все:");
+        line.Clear();
+        circle.Clear();
+        rect.Clear();
+
+        // Рисуем еще что-то
+        Console.WriteLine("\nРисуем новую фигуру:");
+        LineDrawing line2 = new LineDrawing();
+        line2.X1 = 0;
+        line2.Y1 = 0;
+        line2.X2 = 100;
+        line2.Y2 = 100;
+        line2.Draw();
+    }
+}
